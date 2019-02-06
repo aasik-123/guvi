0f43ee48dfc99de20adf4ee222ada0f23fe10d40
aasik-123 <47380412+aasik-123@users.noreply.github.com>
2019-02-06 01:14:07 -0800

    Update chan.java

--- chan.java
+++ chan.java
@@ -2,25 +2,25 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+
+class Even
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	Scanner s=new Scanner(System.in);
-	int a;
-	a=s.nextInt();
-	if(a>0)
-	{
-		System.out.println("Positive");
-	}
-	else if(a==0)
-	{
-		System.out.println("Zero");
-	}
-	else
-	{
-		System.out.println("Negative");
-	}
-	}
+public static void main (String[] args) throws java.lang.Exception
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+if(a<0)
+{
+System.out.println("invalid");
+}
+else if(a%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
 }
